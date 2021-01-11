7a5c0b2510538e88110f08b9e2ec9afb7ed50c80
Justin Thornton <justinathornton@gmail.com>
2021-01-11 18:42:51 -0500

    Set ALT default to blue

--- keyboards/massdrop/alt/keymaps/vulcanmonkey/keymap.c
+++ keyboards/massdrop/alt/keymaps/vulcanmonkey/keymap.c
@@ -51,6 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
+    led_animation_id = 6;
 };
 
 // Runs constantly in the background, in a loop.
